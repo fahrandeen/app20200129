87641fc23fa4c2e4545f280e288fff5e4a655eee
Fahran Deen <fahrandeen17@gmail.com>
2020-02-19 12:54:08 +0530

    Updates

---
+++ precache-manifest.a00f17158a94d83278b00fbc68a083c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bf86c17eb4cc03a9f3951cf5cf5727",
+    "url": "/app20200129/index.html"
+  },
+  {
+    "revision": "23c74ec49cb7063b8da3",
+    "url": "/app20200129/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "aa895355c92f4b21d23f",
+    "url": "/app20200129/static/css/main.a298e1e7.chunk.css"
+  },
+  {
+    "revision": "23c74ec49cb7063b8da3",
+    "url": "/app20200129/static/js/2.32653440.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/app20200129/static/js/2.32653440.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa895355c92f4b21d23f",
+    "url": "/app20200129/static/js/main.0d36ebb2.chunk.js"
+  },
+  {
+    "revision": "67ae21140839739103a7",
+    "url": "/app20200129/static/js/runtime-main.b1bf3359.js"
+  }
+]);
